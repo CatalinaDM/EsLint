2199e6ae1be1855cdb92dcae6f1de2ec759e7ae9
CatalinaDM <catidelgadomanzano1@gmail.com>
2026-06-09 20:24:54 -0600

    feat: prueba deploy a Vercer 09 junio 2026

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Prueba Deploy en Vercel por segunda vez Catalina Delgado </h1>
+          <h1>Prueba Deploy en Vercel por tercera vez Catalina Delgado </h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
